2938ac118b004bd5f6fa5f727ba4fbf66029dad0
Lev Kravinsky <kravinskylev@gmail.com>
2015-09-04 13:05:16 -0600

    add example

--- schedule.rb
+++ schedule.rb
@@ -4,6 +4,10 @@ end
 class User
 end
 
+# output
+# please enter all people you'd like on this week's schedule, with their preferences for each day.
+# ex - [["adam sandler", [1, 0, 2, 3, 0]], ["jonah hill", [0, 1, 3, 2, 0]]]
+
 # input
 # [["sarah", [2,3,0,0,1]], ["lev", [1,0,0,2,3]]]
 
