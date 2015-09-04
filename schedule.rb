a144f804cf44639c284e2da7ed4ba47e3ea9b869
Lev Kravinsky <kravinskylev@gmail.com>
2015-09-04 13:44:27 -0600

    add all potential unique schedules w/pairings

--- schedule.rb
+++ schedule.rb
@@ -1,12 +1,61 @@
-class Schedule
+class Calender
+  def self.potential_schedules
+    p %w[Su Mo Tu We Th Fr Sa].product(User.all_pairs)
+  end
+
+  # def days
+  #   {"Su" => [], "Mo" => [], "Tu" => [], "We" => [], "Th" => [], "Fr" => [], "Sa" => []}
+  # end
+
+  def self.rate_potential_schedules
+    schedule_score = {}
+    potential_schedules.each do |schedule|
+      schedule_score[scedule] = schedule.score
+    end
+  end
 end
 
 class User
+  attr_accessor :preferences,
+                :name
+
+  @@users_collection = []
+
   def initialize(name, preferences)
     @name = name
     @preferences = preferences
+    @@users_collection << self
   end
+
+  def self.all
+    @@users_collection
+  end
+
+  def self.all_pairs
+    pairs = []
+    all.each do |user|
+      all.length.times do |num|
+      pairs << [user, all[num]]
+      end
+    end
+    unique_pairs = pairs.map { |pair| pair.sort_by { |user| user.name } }
+    .uniq
+    .reject { |pair| pair if pair[0] == pair[1] }
+    unique_pairs
+  end
+
 end
+
+User.new("lev", [1])
+User.new("sar", [1])
+User.new("josh", [1])
+User.new("eli", [1])
+User.new("zell", [1])
+User.new("emily", [1])
+
+User.all_pairs
+
+Calender.potential_schedules
 
 # Iteration 1
 # output
