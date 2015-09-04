4235e2938200f8f23f963dba84e257ceb58c2a47
Lev Kravinsky <kravinskylev@gmail.com>
2015-09-04 13:02:35 -0600

    first pass at desired IO

---
+++ schedule.rb
@@ -0,0 +1,23 @@
+class Schedule
+end
+
+class User
+end
+
+# input
+# [["sarah", [2,3,0,0,1]], ["lev", [1,0,0,2,3]]]
+
+# output
+# is sarah a noob? (Y/N)
+# is sarah okay to work on Shabbat? (Y/N)
+
+# input
+# no
+# yes
+
+# this is repeated for all users
+
+# final output
+# okay, here's your schedule:
+# Mo  |  Tu  |  We  |  Th  |  Fr  |  Sa  |  Su
+# sar |  lev | lev  | sar  | sar  | lev  | lev
