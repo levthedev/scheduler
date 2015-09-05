66a8d288c6f2dd178c2b9709c8f2e954e44bee2a
Lev Kravinsky <kravinskylev@gmail.com>
2015-09-05 14:28:17 -0600

    first pass at actual scoring of weeks

--- combinations.rb
+++ combinations.rb
@@ -27,34 +27,47 @@ class User
     unique_pairs
   end
 
-  def assign(top_choice)
-    @work_day = top_choice
-  end
+  # def assign(top_choice)
+  #   @work_day = top_choice
+  # end
 end
 
-User.new("lev", [0, 1])
-User.new("sar", [2, 3])
-User.new("josh", [2, 1])
-User.new("eli", [1, 3])
-User.new("zell", [2, 1])
-User.new("emily", [0, 1])
+# week = []
+# 7.times do |n|
+#   random_user = User.all.sample
+#   top_choice  = random_user.preferences.index(1)
+#   week        << random_user
+#   random_user.assign(top_choice)
+# end
+# week.map {|u| u.name}
 
-week = []
-7.times do |n|
-  random_user = User.all.sample
-  top_choice  = random_user.preferences.index(1)
-  week        << random_user
-  random_user.assign(top_choice)
-end
-week.map {|u| u.name}
-
-User.all_pairs.combination(7).to_a.map {|week| Schedule.score(week)}
+User.new("lev",   [0,1,2,3,0,0,0])
+User.new("sar",   [2,3,0,0,1,0,0])
+User.new("josh",  [0,0,2,0,1,3,0])
+User.new("eli",   [2,0,0,0,1,0,3])
+User.new("zell",  [3,0,1,0,2,0,0])
+User.new("emily", [1,2,0,3,0,0,0])
 
 class Schedule
   def self.score(week)
     count = 0
     week.each_with_index do |pair, index|
-      
+      case pair[0].preferences[index]
+      when 3 then count += 5
+      when 2 then count += 3
+      when 1 then count += 1
+      when 0 then count -= 5
+      end
+
+      case pair[1].preferences[index]
+      when 3 then count += 5
+      when 2 then count += 3
+      when 1 then count += 1
+      when 0 then count -= 5
+      end
     end
+    week.zip([count])
   end
 end
+
+User.all_pairs.combination(7).to_a.map { |week| Schedule.score(week) }
