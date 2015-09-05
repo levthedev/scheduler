1e48309a3a0026b1facc043e4b53ccb93e377507
Lev Kravinsky <kravinskylev@gmail.com>
2015-09-05 15:03:09 -0600

    passing naive solution

--- combinations.rb
+++ combinations.rb
@@ -48,15 +48,30 @@ User.new("eli",   [2,0,0,0,1,0,3])
 User.new("zell",  [3,0,1,0,2,0,0])
 User.new("emily", [1,2,0,3,0,0,0])
 
+
+# lev/sar
+# eli/lev
+# lev/zell
+# emily/lev
+# josh/zell
+# emily/josh
+# eli/zell
+
+
 class Schedule
+  def self.scores
+    @@scores
+  end
+
   def self.score(week)
     count = 0
+    @@scores ||= Hash.new(0)
     week.each_with_index do |pair, index|
       case pair[0].preferences[index]
       when 3 then count += 5
       when 2 then count += 3
       when 1 then count += 1
-      when 0 then count -= 5
+      when 0 then count -= 2
       end
 
       case pair[1].preferences[index]
@@ -66,8 +81,23 @@ class Schedule
       when 0 then count -= 5
       end
     end
-    week.zip([count])
+    @@scores[week] += count
+  end
+
+  def self.best
+    best = @@scores.max_by(3) do |week, score|
+      score
+    end
+    best.each do |week|
+      week.first.each { |pair| printf "#{pair[0].name}/#{pair[1].name}\n\n"}
+    end
+    #best.first.first
   end
 end
 
-User.all_pairs.combination(7).to_a.map { |week| Schedule.score(week) }
+User.all_pairs.combination(7).to_a.map { |week| Schedule.score(week)}
+Schedule.best
+
+#User.all_pairs.combination(7).to_a.length
+#User.all_pairs.combination(7).to_a.map { |week| Schedule.score(week) }  # => [[[[#<User:0x007f9b4481eb88 @name="lev", @preferences=[0, 1, 2, 3, 0, 0, 0]>, #<User:0x007f9b4481de68 @name="sar", @preferences=[2, 3, 0, 0, 1, 0, 0]>], -26], [[#<User:0x007f9b4481d0f8 @name="josh", @preferences=[0, 0, 2, 0, 1, 3, 0]>, #<User:0x007f9b4481eb88 @name="lev", @preferences=[0, 1, 2, 3, 0, 0, 0]>], nil], [[#<User:0x007f9b4481c068 @name="eli", @preferences=[2, 0, 0, 0, 1, 0, 3]>, #<User:0x007f9b4481eb88 @name="lev", @preferences=[0, 1, 2, 3, 0, 0, 0]>], nil], [[#<User:0x007f9b4481eb88 @name="lev", @preferences=[0, 1, 2, 3, 0, 0, 0]>, #<User:0x007f9b448b4c28 @name="zell", @preferences=[3, 0, 1, 0, 2, 0, 0]>], nil], [[#<User:0x007f9b4481d0f8 @name="josh", @preferences=[0, 0, 2, 0, 1, 3, 0]>, #<User:0x007f9b4481de68 @name="sar", @preferences=[2, 3, 0, 0, 1, 0, 0]>], nil], [[#<User:0x007f9b4481c068 @name="eli", @preferences=[2, 0, 0, 0, 1, 0, 3]>, #<User:0x007f9b4481de68 @name="sar", @preferences=[2, ...
+#User.all_pairs.combination(7).to_a.map {|e| Schedule.score(e) }
